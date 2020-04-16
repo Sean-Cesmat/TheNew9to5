4d230b51147d456223aa6cd0de85e5c01061043c
Sean Cesmat <sean.cesmat@gmail.com>
2020-04-16 11:07:18 -0700

    fixed the twitch stream fitting in the tv on safari

--- components/Twitch.vue
+++ components/Twitch.vue
@@ -1,44 +1,44 @@
 <template>
     <div class="twitch">
-        <!-- <img src="~/assets/img/meeting-room.jpg" />
-        <iframe src="https://player.twitch.tv/?channel=subsetgetsit" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe> -->
-        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 3195 2412">
-            <image width="3195" height="2412" href="~/assets/img/meeting-room-wide.jpg" />
-            <foreignObject x="1247" y="869" width="717" height="407">
-                <div xmlns="http://www.w3.org/1999/xhtml">
-                    <iframe src="https://player.twitch.tv/?channel=subsetgetsit" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
-                </div>
-            </foreignObject>
-        </svg>
+        <div class="twitch__inner">
+            <img src="~/assets/img/meeting-room-wide.jpg" />
+            <iframe src="https://player.twitch.tv/?channel=subsetgetsit" :style="{ position: 'absolute', top: iFrameLocation.y + '%', left: iFrameLocation.x + '%', width: iFrameLocation.w + '%', height: iFrameLocation.h + '%' }" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+        </div>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            iFrameLocation: {
+                x: 39.0297339593, // (1247 / 3195) * 100
+                y: 36.0281923715, // (869 / 2412) * 100
+                w: 22.441314554, // (717 / 3195) * 100
+                h: 16.8739635158, // (407 / 2412) * 100
+            },
+        }
+    },
+}
+</script>
 <style lang="scss" scoped>
 .twitch {
     width: 100%;
     overflow: hidden;
+    position: relative;
     img {
         width: 100%;
+        height: auto;
     }
-    svg {
+    &__inner {
+        position: relative;
         min-width: 3500px;
         margin-top: -600px;
         margin-left: calc(50% - 10px);
         transform: translateX(-50%);
-        // @media screen and (max-width: 1023px) {
-        //     min-width: 3300px;
-        // }
         @media screen and (max-width: $tn95-screen-max-lg) {
             min-width: 400vw;
             margin-top: calc(-1 * 100vw + 200px);
         }
-    }
-    svg div {
-        width: 100%;
-        height: 100%;
-    }
-    svg iframe {
-        width: 100%;
-        height: 100%;
     }
 }
 </style>
