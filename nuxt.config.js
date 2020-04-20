d21bf802f80fb0041e26db25f3827680a11af75d
Sean Cesmat <sean.cesmat@gmail.com>
2020-04-20 11:12:30 -0700

    added google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,12 @@ export default {
         '@nuxtjs/eslint-module',
         // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
         '@nuxtjs/tailwindcss',
+        [
+            '@nuxtjs/google-analytics',
+            {
+                id: 'UA-164027777-1',
+            },
+        ],
     ],
     /*
      ** Nuxt.js modules
